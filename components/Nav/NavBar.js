998941edf54c3a59ff3ffa54944847d347d13bbd
KUSHAGRA-JAISWAL <kushagrajaiswal00@gmail.com>
2023-02-25 19:39:55 +0530

    Add styling to the index page.

---
+++ components/Nav/NavBar.js
@@ -0,0 +1,23 @@
+import Image from "next/image";
+import styles from "../../styles/Nav.module.css"
+export const NavBar = props => {
+    return (
+        <>
+        <div className={styles.navContainer}>
+        <Image style={{opacity: 0.50}}
+            src = "/share.png"
+            width = "35"
+            height = "25"
+            /> 
+            <h3 className={styles.navTitle} >File Share</h3>
+            <ul className={styles.navList}>
+                <li className={styles.navButton}><a href="#">Features</a></li>
+                <li className={styles.navButton}><a href="#">Services</a></li>
+                <li className={styles.navButton}><a href="#">About</a></li>
+            </ul>
+            <button className={styles.navContact}>Contacts</button>
+        </div>
+            
+        </>
+    )
+}
--- pages/_document.js
+++ pages/_document.js
@@ -1,12 +1,29 @@
+import { NavBar } from '@/components/Nav/NavBar'
 import { Html, Head, Main, NextScript } from 'next/document'
+import Image from "next/image";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
       <body>
+      <div style={{
+        zIndex: -1,
+        position: "fixed",
+        width: "100vw",
+        height: "100vh",
+        opacity: 0.50
+       
+      }}>
+      <Image src="/bg2.jpg" alt="cat Background" layout="fill" objectFit='cover' />
+      </div>
+        <nav>
+          <NavBar />
+        </nav>
+        <div className="mainNext">
         <Main />
         <NextScript />
+        </div>
       </body>
     </Html>
   )
